61f5a6feb422321dfca2cd6f76d3acce8ef06538
Tettey-Lowor <frederickatettey10@gmail.com>
2024-02-27 17:05:19 +0000

    new agro changes

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import FooterComponent from "../components/FooterComponent";
+import AboutComponent from "../components/AboutComponent";
+
+
+function AboutPage () {
+    return(
+        <div>
+            <>
+            <AboutComponent/>
+            <FooterComponent/>
+            </>
+        </div>
+    )
+
+    };
+
+    export default AboutPage;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import NavbarComponent from "../components/NavbarComponent";
 import HeroComponent from "../components/HeroComponent";
+import AboutComponent from "../components/AboutComponent";
+import FooterComponent from "../components/FooterComponent";
 
 function HomePage() {
   return (
@@ -8,6 +10,8 @@ function HomePage() {
       <>
         {/* <NavbarComponent /> */}
         <HeroComponent />
+        <AboutComponent />
+        <FooterComponent />
       </>
     </div>
   );
