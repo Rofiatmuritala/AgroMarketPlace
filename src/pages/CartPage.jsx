4860d45d2b70f1b8953101466cb91b555d0a61f7
abdulazeezcoding <abdulazizmohammed5103@gmail.com>
2024-02-29 17:41:35 +0000

    CartPage

---
+++ src/pages/CartPage.jsx
@@ -0,0 +1,21 @@
+
+import React from "react";
+
+export default function () {
+    return (
+        <>
+            <section className="cart-card">
+                <div>
+                    <h4>Cart</h4>
+                    <span>Remove all</span>
+                </div>
+
+                <div className="product-row">
+                    <img src="" alt="" />
+                    
+                </div>
+
+            </section>
+        </>
+    );
+}
